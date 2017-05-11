7a6eaaa34027a84eb2c1d9a3fff17f7edf6c9633
leilei1116 <594535940@qq.com>
2017-05-11 22:50:38 +0800

    陈蕾 搜索 、私信 、个人账号管理、多个activity之间的滑动点击切换

--- app/src/main/java/com/example/wangyan/oh_my_news_android_client/activities/SearchActivity.java
+++ app/src/main/java/com/example/wangyan/oh_my_news_android_client/activities/SearchActivity.java
@@ -1,15 +1,160 @@
 package com.example.wangyan.oh_my_news_android_client.activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import com.example.wangyan.oh_my_news_android_client.R;
+import com.example.wangyan.oh_my_news_android_client.okhttp.CommonOkHttpClient;
+import com.example.wangyan.oh_my_news_android_client.okhttp.exception.OkHttpException;
+import com.example.wangyan.oh_my_news_android_client.okhttp.listener.ResponseDataHandle;
+import com.example.wangyan.oh_my_news_android_client.okhttp.listener.ResponseDataListener;
+import com.example.wangyan.oh_my_news_android_client.okhttp.request.CommonRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SearchActivity extends AppCompatActivity {
+    Context context;
+//    private String[] index= new String[]{"aa","bb","cc","dd"};
+//    private String[] s= {"时尚", "科技", "医学", "财经"};
+    private SearchView search;
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        context=this;
+        final LayoutInflater layoutInflater=LayoutInflater.from(context);
+        search = (SearchView) findViewById(R.id.searchView);
+        list = (ListView) findViewById(R.id.listView);
+        LinearLayout linearLayout=(LinearLayout)layoutInflater.inflate(R.layout.itemlayout,null);
+//        List<Map<String,Object>> t=new ArrayList<>();
+//        for(int i=0;i<4;i++) {
+//            Map<String, Object> map = new HashMap<>();
+//            map.put("index",index[i] );
+//            map.put("title",s[i]);
+//            t.add(map);
+//        }
+//        SimpleAdapter adapter=new SimpleAdapter(context,t,R.layout.itemlayout,new String[]{"index","title"},new int[]{R.id.index,R.id.text});
+//        list.setAdapter(adapter);
+//        //启用过滤
+//        list.setTextFilterEnabled(true);
+        //显示搜索按钮
+        search.setSubmitButtonEnabled(true);
+
+
+
+        // 设置搜索文本监听
+        search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            // 当点击搜索按钮时触发该方法
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Intent intent = new Intent();
+                // intent.setClass(SearchActivity.this, MainActivity.class);
+                // intent.putExtra("id", query);
+                //startActivity(intent);
+                postRequest(query);
+                // Toast.makeText(SearchActivity.this, "选择的是;" + query, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            // 当搜索内容改变时触发该方法
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (!TextUtils.isEmpty(newText)) {
+                    list.setFilterText(newText);
+                } else {
+                    list.clearTextFilter();
+                }
+                return false;
+            }
+        });
+    }
+    private void postRequest(String query){
+        Map<String,Object> params = new HashMap<String,Object>();
+        String url = "/search/showResult";
+
+        params.put("KeyWord",query);
+
+        CommonOkHttpClient.post(CommonRequest.createPostResquest(url,params),new ResponseDataHandle(new ResponseDataListener() {
+            @Override
+            public void onSuccess(Object responseObj) {
+                try {
+                    JSONObject jsonObject = new JSONObject(responseObj.toString());
+                    JSONArray resultList=jsonObject.getJSONArray("resultList");
+                    //System.out.println(resultList);
+                    int len= resultList.length();
+                    final  List<Map<String,Object>> tt=new ArrayList<>();
+                    for(int i = 0; i < len; i++){//遍历JSONArray
+                        JSONObject ob= resultList.getJSONObject(i);
+                        String index=ob.getString("index");
+                        String title=ob.getString("title");
+                        Map<String, Object> map1= new HashMap<>();
+                        map1.put("index",index );
+                        map1.put("title",title);
+                        tt.add(map1);
+
+                        // System.out.println(title);
+                        // System.out.println(ob);
+                    }
+                    SimpleAdapter adapter=new SimpleAdapter(context,tt,R.layout.itemlayout,new String[]{"index","title"},new int[]{R.id.index,R.id.text});
+                    list.setAdapter(adapter);
+                    list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> arg0, View v, int arg2, long arg3) {
+                            System.out.println(arg2);
+                            Map<String,Object> mm =  tt.get(arg2);
+                            String index=(String )mm.get("index");
+                            System.out.println(index);
+                            Intent intent=new Intent();
+                            intent.setClass(SearchActivity.this, DetailActivity.class);
+                            intent.putExtra("articleId",index);
+                            startActivity(intent);
+                        }
+                    });
+//                    if (jsonObject.has("email")) {
+//                        email = jsonObject.getString("email");
+//                       // editText.setText(email);
+//                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Object reasonObj) {
+//                自定义异常，当网络请求失败时可能需要在页面进行显示（-1：网络错误；-2：其他错误）
+
+                OkHttpException exception = new OkHttpException();
+                if (exception.getEcode() == -1 && exception.getEmsg() == null){
+                    Toast.makeText(SearchActivity.this,"网络不稳定",Toast.LENGTH_LONG).show();
+                }
+                if (exception.getEcode() == -2 && exception.getEmsg() == null){
+                    Toast.makeText(SearchActivity.this,"数据格式有误",Toast.LENGTH_LONG).show();
+                }
+            }
+        }));
     }
 }
+
+
+
---
+++ app/src/main/java/com/example/wangyan/oh_my_news_android_client/classes/msgAdapter.java
@@ -0,0 +1,62 @@
+package com.example.wangyan.oh_my_news_android_client.classes;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.wangyan.oh_my_news_android_client.R;
+import com.example.wangyan.oh_my_news_android_client.classes.message;
+
+import java.util.List;
+
+/**
+ * Created by leilei on 2017/5/8.
+ */
+
+public class msgAdapter extends ArrayAdapter<message> {
+    private int resourceId;
+
+    public msgAdapter(Context context, int textViewResourceId, List<message> objects) {
+        super(context, textViewResourceId, objects);
+        resourceId = textViewResourceId;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        message msg = getItem(position);
+        View view;
+        ViewHolder viewHolder;
+        if(convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(resourceId, null);
+            viewHolder = new ViewHolder();
+            viewHolder.leftLayout = (LinearLayout)view.findViewById(R.id.left_layout);
+            viewHolder.rightLayout = (LinearLayout)view.findViewById(R.id.right_layout);
+            viewHolder.leftMsg = (TextView)view.findViewById(R.id.left_msg);
+            viewHolder.rightMsg = (TextView)view.findViewById(R.id.right_msg);
+            view.setTag(viewHolder);
+        } else {
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag();
+        }
+        if(msg.getType() == message.TYPE_RECEIVED) {
+            viewHolder.leftLayout.setVisibility(View.VISIBLE);
+            viewHolder.rightLayout.setVisibility(View.GONE);
+            viewHolder.leftMsg.setText(msg.getContent());
+        } else if(msg.getType() == message.TYPE_SEND) {
+            viewHolder.rightLayout.setVisibility(View.VISIBLE);
+            viewHolder.leftLayout.setVisibility(View.GONE);
+            viewHolder.rightMsg.setText(msg.getContent());
+        }
+        return view;
+    }
+
+    class ViewHolder {
+        LinearLayout leftLayout;
+        LinearLayout rightLayout;
+        TextView leftMsg;
+        TextView rightMsg;
+    }
+}
